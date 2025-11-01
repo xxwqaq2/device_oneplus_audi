355ab07163d1103b91b87d29c1e108e5a794f2ea
Pranav Vashi <neobuddy89@gmail.com>
2025-11-01 17:04:37 +0530

    audi: Enable UDFPS animations
    * crdroid feature
    
    Change-Id: Ic0c7bfed383d17f7badd7e7fbde6eb83be700369
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ TARGET_SCREEN_WIDTH := 1240
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946583411818883.xml
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint3-service.strongbox.nxp \
